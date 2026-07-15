7dd288ae0bcd905e428f5ac03f5e7a5e49ea6ce3
Rainer Simon <rainer@rainersimon.io>
2026-07-15 07:06:08 +0200

    Cleanup

--- src/pages/reconstruction/reconstruction-store.ts
+++ src/pages/reconstruction/reconstruction-store.ts
@@ -3,10 +3,6 @@ import type { ReconstructionCanvas } from '@/types';
 
 interface ReconstructionStore {
 
-  hoveredCanvas?: ReconstructionCanvas;
-
-  setHoveredCanvas(hovered?: ReconstructionCanvas): void;
-
   selection: ReconstructionCanvas[];
 
   setSelection(selected: ReconstructionCanvas[] | ((current: ReconstructionCanvas[]) => ReconstructionCanvas[])): void;
@@ -15,11 +11,7 @@ interface ReconstructionStore {
 
 export const useReconstructionStore = create<ReconstructionStore>()(set => ({
 
-  hoveredCanvas: undefined,
-
   selection: [],
-
-  setHoveredCanvas: hoveredCanvas => set({ hoveredCanvas }),
 
   setSelection: arg => set(({ selection }) => ({
     selection: typeof arg === 'function' ? arg(selection) : arg
