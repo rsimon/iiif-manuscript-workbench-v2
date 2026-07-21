d80c83e3fc9d9230b86ab1e21d08255d3376f540
Rainer Simon <rainer@rainersimon.io>
2026-07-21 14:10:49 +0200

    Started 'preview' stage scaffolding

---
+++ src/pages/preview/metadata/metadata.tsx
@@ -0,0 +1,5 @@
+export const Metadata = () => {
+
+  return null;
+
+}
--- src/pages/preview/preview.tsx
+++ src/pages/preview/preview.tsx
@@ -1,8 +1,40 @@
+import { useState } from 'react';
+import { Panel, Separator } from 'react-resizable-panels';
+import { AnimatedPanel, AnimatedPanelGroup } from '@/components/animated-panel';
+import { Viewer } from './viewer';
+import { ThumbnailStrip } from './thumbnail-strip';
+import { Metadata } from './metadata/metadata';
+
 export const Preview = () => {
+  const [isMetadataOpen, setIsMetadataOpen] = useState(false);
 
   return (
     <main className="bg-muted grow">
-      
+      <AnimatedPanelGroup className="flex grow h-full min-h-0">
+        <Panel
+          minSize={240}
+          defaultSize={300}
+          className="border-r">
+          <ThumbnailStrip />
+        </Panel>
+
+        <Separator />
+
+        <Panel>
+          <Viewer />
+        </Panel>
+
+        <Separator />
+        
+        <AnimatedPanel 
+          open={isMetadataOpen}
+          onOpenChange={setIsMetadataOpen}
+          minSize={40}
+          openSize={300}
+          className="bg-white border-l">
+          <Metadata />
+        </AnimatedPanel>
+      </AnimatedPanelGroup>
     </main>
   )
 
